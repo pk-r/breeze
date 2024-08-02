a08b512d8400272d6a8f2c3532da41270bd30e97
Pouria Khakpour <pouria@pino.rest>
2024-08-02 13:26:36 +0330

    Add cobra and viper

--- cmd/breezed/breezed.go
+++ cmd/breezed/breezed.go
@@ -1,7 +1,52 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+	"os"
+)
+
+var cfgFile string
+
+var rootCmd = &cobra.Command{
+	Use:   "breezed",
+	Short: "A brief description of your application",
+	Long: `A longer description that spans multiple lines and likely contains
+examples and usage of using your application.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("Running breezed...")
+	},
+}
+
+// versionCmd represents the version command
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "Prints the version number of breezed",
+	Long:  `This command allows you to print the version number of breezed.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("Version:", viper.GetString("VERSION"))
+	},
+}
+
+func init() {
+	cobra.OnInitialize(initConfig)
+	rootCmd.AddCommand(versionCmd)
+}
+
+func initConfig() {
+	viper.SetEnvPrefix("BREEZED")
+	viper.AutomaticEnv()
+
+	// Read in environment variables from the .env file
+	if err := viper.ReadInConfig(); err == nil {
+		fmt.Printf("Using config file: %s\n", viper.ConfigFileUsed())
+	}
+}
 
 func main() {
-	fmt.Println("Hello, Breeze!")
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 }
